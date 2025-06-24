e106f2820479f66cce7cdecead05169d746c8274
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2025-06-24 12:19:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -712,7 +712,6 @@ def health_check():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
         create_admin_user()
     
     port = int(os.environ.get('PORT', 5000))
